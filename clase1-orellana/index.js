2aaa12b7a297b9be470aebf834f67bc195393e53
agustina <agucha.orellana1@gmail.com>
2021-04-19 23:00:10 -0300

    desafio clase uno

---
+++ clase1-orellana/index.js
@@ -0,0 +1,12 @@
+// Ejemplo uno
+let numeroUno = Number(prompt("ingrese un numero"));
+let numeroDos = 10;
+
+console.log( 'El resultado es ' + (numeroUno + numeroDos));
+
+// Ejemplo dos
+let nombre = prompt('ingrese el nombre');
+let apellido = prompt('ingrese el apellido');
+ 
+alert('Bienvenido ' + nombre  + ' ' + apellido);
+
