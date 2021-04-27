6f125a08a3ddc717a5c2ddbec2ba5db4426e2fa7
agustina <agucha.orellana1@gmail.com>
2021-04-26 23:11:26 -0300

    exercise if else

---
+++ clase2/index.js
@@ -0,0 +1,28 @@
+// EJEMPLO 
+
+let edad = prompt('ingrese su edad');
+
+if(edad >= 18){
+    alert('es mayor de edad');
+}else{
+    alert('no es mayor de edad')
+};
+
+// EJEMPLO DOS
+let saludo = prompt('ingrese su saludo');
+
+if(saludo == 'Hola'){
+    alert('Hola como estas?')
+}else{
+    alert('No me saludaste')
+};
+
+// EJEMPLO TRES
+let numero = prompt('ingrese un numero');
+
+if(numero >= 10 && numero <= 50){
+    console.log('Tu numero esta entre 10 y 50')
+
+}else{
+    console.log('Tu numero no cumple las condiciones')
+};
